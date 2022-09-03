587094460c82827db4b823c5576892a6062c5a15
lokideos <archmage@hey.com>
2022-09-03 16:17:50 +0300

    chore: require settings

--- config/boot.rb
+++ config/boot.rb
@@ -3,5 +3,7 @@
 require 'bundler/setup'
 Bundler.require(:default, ENV.fetch('PROJECT_ENV', :development).to_sym)
 
+require_relative 'initializers/config'
+
 require_relative '../system/container'
 Container.finalize!

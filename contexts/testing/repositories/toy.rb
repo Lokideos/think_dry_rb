4786a8feacc815a7f9fcaa1c757e252102aa9dac
lokideos <archmage@hey.com>
2022-09-03 19:48:29 +0300

    feat: add test toy command

--- contexts/testing/repositories/toy.rb
+++ contexts/testing/repositories/toy.rb
@@ -20,6 +20,11 @@ module Testing
       def find_not_tested_for_account(account_id)
         root.where(account_id: account_id, tested: false).to_a
       end
+
+      def assign_account(account_id, toy_id)
+        root.by_pk(toy_id).update(account_id: account_id)
+        find(toy_id)
+      end
     end
   end
 end
